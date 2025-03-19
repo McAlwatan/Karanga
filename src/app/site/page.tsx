7b86f7d68e11447dc1ac31fac4356d27a3858991
mhuma <Menard@Menard.field.local>
2025-03-19 10:29:30 +0300

    git fix

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,10 +1,13 @@
 import GradientText from "@/components/global/gradient-text";
+import MyImage from "@/components/global/image";
 
 export default function Home() {
   return (
     <div className="max-h-screen flex flex-col">
       <main className="flex-1 justify-center w-full p-4 mt-4">
-        
+        <MyImage>
+          
+        </MyImage>
         <GradientText
           className="text-[90px] font-semibold leading-none"
           element="H2"
